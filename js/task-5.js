cf7b3353f62c887ae0bb461e708322180a442853
kearlock <kearlock@gmail.com>
2025-06-21 21:58:58 +0300

    dropdown menu

--- js/task-5.js
+++ js/task-5.js
@@ -1,7 +1,7 @@
 try {
-    console.log('This message will not appear in the console');
-cos value = 5;
+  console.log("This message will not appear in the console");
+  JSON.parse("cos value = 5");
 } catch (error) {
-    console.log(error)
-    console.error('An error occurred:', error.message);
+  console.log(error);
+  console.error("An error occurred:", error.message);
 }
